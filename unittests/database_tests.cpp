b0762db0c84841605a77ac2aee38b35e1cb0f181
Brian Johnson <johnsonb@objectcomputing.com>
2021-01-13 16:12:21 -0600

    Just testing

--- unittests/database_tests.cpp
+++ unittests/database_tests.cpp
@@ -17,34 +17,6 @@ namespace bfs = boost::filesystem;
 
 BOOST_AUTO_TEST_SUITE(database_tests)
 
-   // Simple tests of undo infrastructure
-   BOOST_AUTO_TEST_CASE(undo_test) {
-      try {
-         TESTER test;
-
-         // Bypass read-only restriction on state DB access for this unit test which really needs to mutate the DB to properly conduct its test.
-         eosio::chain::database& db = const_cast<eosio::chain::database&>( test.control->db() );
-
-         auto ses = db.start_undo_session(true);
-
-         // Create an account
-         db.create<account_object>([](account_object &a) {
-            a.name = name("billy");
-         });
-
-         // Make sure we can retrieve that account by name
-         auto ptr = db.find<account_object, by_name>(name("billy"));
-         BOOST_TEST(ptr != nullptr);
-
-         // Undo creation of the account
-         ses.undo();
-
-         // Make sure we can no longer find the account
-         ptr = db.find<account_object, by_name>(name("billy"));
-         BOOST_TEST(ptr == nullptr);
-      } FC_LOG_AND_RETHROW()
-   }
-
    // Test the block fetching methods on database, fetch_bock_by_id, and fetch_block_by_number
    BOOST_AUTO_TEST_CASE(get_blocks) {
       try {
